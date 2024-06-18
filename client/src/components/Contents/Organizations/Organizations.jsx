e017cd799e1e533a455b2b3f400532c806749fd1
Tharun Kumar P <tharunkp41@gmail.com>
2024-06-18 11:13:43 +0530

    new org

--- client/src/components/Contents/Organizations/Organizations.jsx
+++ client/src/components/Contents/Organizations/Organizations.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Grid, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import Sidebar from './Sidebar';
 import MainContent from './MainContent';
 
